3085466952c2c735f22187a976e3e40667fcd2db
Andrievich Regina <andrievichregina@Mac-mini-Andrievich.local>
2025-03-01 13:10:52 +1000

    fixed App

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { WeatherChart } from './components/WeatherChart';
 import { SearchBar } from './components/SearchBar';
 import { Controls } from './components/Controls';
